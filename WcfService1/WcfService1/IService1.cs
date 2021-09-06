bd389943adfbe37c67b9e8180784b3b88edfde06
yashwanthsiva <87614807+yashwanthsiva@users.noreply.github.com>
2021-09-06 20:34:40 +0530

    Add files via upload

---
+++ WcfService1/WcfService1/IService1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        double Add(double num1, double num2);
+        [OperationContract]
+        double Sub(double num1, double num2);
+        [OperationContract]
+        double Divide(double num1, double num2);
+        [OperationContract]
+        double Multiply(double num1, double num2);
+
+    }
+
+
+    
+   
+}
